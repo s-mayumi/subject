b3fdf9180073ed791cef88efb27836f9dd59db8f
Daichi Kanke <kanke@tam-tam.co.jp>
2017-05-09 11:53:08 +0900

    jsの処理を修正

--- 01-calculator/htdocs/assets/js/calculator.js
+++ 01-calculator/htdocs/assets/js/calculator.js
@@ -9,44 +9,67 @@ $(function(){
   var keepNumTotal   = 0;                  //計算結果
   var flag;
 
+  // 数値キーを押した時
   inputNum.on('click', function(){
-    var getNum = $(this).val(); //押された数字の値を getNum に代入
-    valResult  = parseInt(getNum);
 
-    if( flag == '+' ){
-      keepNumTotal = keepNumTotal += valResult;
-    } else if( flag == '-' ){
-      keepNumTotal = keepNumTotal -= valResult;
-    } else if( flag == '*' ){
-      keepNumTotal = keepNumTotal *= valResult;
-    } else if( flag == '/' ){
-      keepNumTotal = keepNumTotal /= valResult;
+    var getNum;
+
+    if ( valResult === 0 ) {
+        getNum = $(this).val(); //押された数字の値を getNum に代入
     } else {
-      keepNumTotal = valResult;
+        getNum = valResult + $(this).val(); //画面にあった数字と押された数字を文字列結合して getNum に代入
     }
 
+    valResult = parseInt(getNum);
+
     output.val(valResult);
-    console.log(flag);
-    console.log(keepNum);
-    console.log(valResult);
   });
 
+
+  // 記号ボタンを押した時
   buttonSymbol.on('click', function(){
+    // フラグを立てる
     flag = $(this).val();
-    keepNum = parseInt(valResult); //記号を押す前の数字を keepNum に代入
-    // console.log(flag);
-    // console.log(keepNum);
+
+    // 記号を押す前の数字を keepNum に代入
+    keepNum = parseInt(valResult);
+
+    // 数値の入力値を0に戻す
+    valResult = 0;
   });
 
-  buttonEqual.on('click',function(){
+
+  // イコールボタンを押した時
+  buttonEqual.on('click', function(){
+    switch ( flag ){
+      case '+':
+        keepNumTotal = keepNum + valResult;
+        break;
+      case '-':
+        keepNumTotal = keepNum - valResult;
+        break;
+      case '*':
+        keepNumTotal = keepNum * valResult;
+        break;
+      case '/':
+        keepNumTotal = keepNum / valResult;
+        break;
+      default:
+        keepNumTotal = valResult;
+        break;
+    }
+
     output.val(keepNumTotal);
+    valResult = 0;
   });
 
+
+  // クリアボタンを押した時
   buttonClear.on('click', function(){
     output.val(0);
-    keepNum = '0';
+    keepNum = 0;
+    valResult = 0;
     flag = null;
-    console.log(keepNum);
   });
 });
 
